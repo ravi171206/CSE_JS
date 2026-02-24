4e8322cda0c3ce226ca9ae19edf174dfafb026fd
ravi171206 <ravi7351vv@gmail.com>
2026-02-24 15:33:46 +0530

    javascriptlec2

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log("Hello World");
-document.write("hello world")
+document.write("hello world");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+let greeting = "Hello, World!";
+console.log(greeting);
+let name; 
+console.log(name);
+//array
+var numberArray=[1,2,3];
+//object
+var person={
+    name:'ravi',
+    age:'19',
+    title:'student',
+}
