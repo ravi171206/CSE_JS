c03acf51d5245a359ed0eef35ef6b276518064f9
ravi171206 <ravi7351vv@gmail.com>
2026-02-24 16:24:26 +0530

    jslec3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Hello World");
-document.write("hello world");
+// document.write("hello world");
 var num=10;
 console.log(num);
 console.log(typeof(num));
@@ -15,3 +15,28 @@ var person={
     age:'19',
     title:'student',
 }
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+const a = 6, b = 5;
+console.log(a==4 && b==5); 
+console.log(a==6 || b==5); 
+//for loop
+var i;
+for(i=0;i<3;i=i+1){
+    console.log(i);
+}
+// var count;
+// for(count=0;count<10;count++){
+//     document.write("current count:" +count);
+// }
+
+let e=23,d=12;
+console.log("e+d="
+);
+result=e+d;
+console.log(result);
+
+
+
