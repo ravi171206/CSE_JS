bdf8d570860549153bd8d6a8c6b308ac071cc0f8
ravi171206 <ravi7351vv@gmail.com>
2026-03-17 16:28:31 +0530

    htmldom

---
+++ dom.js
@@ -0,0 +1,33 @@
+//1.javascript can change the all the hmtl elments in the page
+//2.js can change  all the html attributes of the htmlm  page
+//3. js can chanbge all the css styles of the pages
+//4. js can remove all the existing elments and attributes of the html page
+//5. js has only inline cell
+//6.js can react to all the html events of the page
+//7. js can create new html elements
+
+// let x= document.getElementById("demo");
+// x.innerHTML="  ";
+//let y = document.getElementsByClassName("demo")
+//y[0].innerHTML =" ";
+// let x= document.getElementById("demo");
+// document.getElementByClassName(" ");
+// document.getElementByTagName( );
+// document.quarySelector(" ");
+// document.quarySelectorAll( );
+//  let x=document.getElementById("demo").getAttribute("id");
+//   let x=document.getElementById("demo").setAttribute("class","democlass");
+// document.getElementById("demo").style.color="blue";
+// let y= document.createElement("h2");
+// let par= document.getElementsByTagName("body");
+// par[0].append(Y);
+
+//element.remove();
+//Mouse Events: click, dblclick, mousemove, mouseover, mouseout
+// Keyboard Events: keydown, keypress, keyup
+// Form Events: submit, change, focus, blur
+// Window Events: load, resize, scroll
+ function onclick() {
+            alert("Mouse move over");
+        } 
+        
--- function.js
+++ function.js
@@ -6,6 +6,15 @@
 // }
 
 // arrow function is an anonymous function (function in  a variable)
-let add =(x,y)=>{return x+y;}
-console.log(add(2,4));
-console.log(add(4,4));
+// let add =(x,y)=>{return x+y;}
+// console.log(add(2,4));
+// console.log(add(4,4)); 
+//object is represnted by curly brackets 
+// var person2= new Object();
+// person2.name='ravi';
+// person2.age=19;
+// person2.isMarried=true;
+// person2.address={};
+// person2.address.street="varanasi";
+// person2['address']['flatNO']=33;
+
