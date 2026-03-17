186d216412100e6abc418c14614bb69019bce7a7
ravi171206 <ravi7351vv@gmail.com>
2026-03-17 15:30:20 +0530

    jsfunction

---
+++ function.js
@@ -0,0 +1,11 @@
+// function myFunction(){
+//     alert
+// }
+// function sayHello(name,age){
+//     document.write(name+"is"+age+"years old.");
+// }
+
+// arrow function is an anonymous function (function in  a variable)
+let add =(x,y)=>{return x+y;}
+console.log(add(2,4));
+console.log(add(4,4));
--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-console.log("Hello World");
-// document.write("hello world");
-var num=10;
-console.log(num);
-console.log(typeof(num));
-let greeting = "Hello, World!";
-console.log(greeting);
-let name; 
-console.log(name);
-//array
-var numberArray=[1,2,3];
-//object
-var person={
-    name:'ravi',
-    age:'19',
-    title:'student',
-}
-const sym1=Symbol(4)
-console.log(sym1);
-const sym2=Symbol(4);
-console.log(sym2);
-const a = 6, b = 5;
-console.log(a==4 && b==5); 
-console.log(a==6 || b==5); 
-//for loop
-var i;
-for(i=0;i<3;i=i+1){
-    console.log(i);
-}
-// var count;
-// for(count=0;count<10;count++){
-//     document.write("current count:" +count);
+// console.log("Hello World");
+// // document.write("hello world");
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// let greeting = "Hello, World!";
+// console.log(greeting);
+// let name; 
+// console.log(name);
+// //array
+// var numberArray=[1,2,3];
+// //object
+// var person={
+//     name:'ravi',
+//     age:'19',
+//     title:'student',
 // }
+// const sym1=Symbol(4)
+// console.log(sym1);
+// const sym2=Symbol(4);
+// console.log(sym2);
+// const a = 6, b = 5;
+// console.log(a==4 && b==5); 
+// console.log(a==6 || b==5); 
+// //for loop
+// var i;
+// for(i=0;i<3;i=i+1){
+//     console.log(i);
+// }
+// // var count;
+// // for(count=0;count<10;count++){
+// //     document.write("current count:" +count);
+// // }
 
-let e=23,d=12;
-console.log("e+d="
-);
-result=e+d;
-console.log(result);
+// let e=23,d=12;
+// console.log("e+d="
+// );
+// result=e+d;
+// console.log(result);
+
+// // maximum out of 3  numbers
+// const num1=12,num2=16,num3=9;
 
 
-
+var grade='A';
+console.log("Entering switch block");
+switch(grade){
+    case'A':
+}
